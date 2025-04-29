327111910f8183cd8c09859dba24a67d3fce8020
ykztenoks <danielskonetzky7@gmail.com>
2025-04-29 16:56:43 -0300

    fix week 4 day 1

--- module-1/week-4/day-1/OOP.js
+++ module-1/week-4/day-1/OOP.js
@@ -1,79 +1,78 @@
-// //creating/declaring the class
-// class Person {
-//   constructor(name, age, country) {
-//     this.name = name
-//     this.age = age
-//     this.country = country
-//   }
-//   sayHello() {
-//     console.log("hello! my name is " + this.name)
-//   }
-// }
+//creating/declaring the class
+class Person {
+  constructor(name, age, country) {
+    this.name = name
+    this.age = age
+    this.country = country
+  }
+  sayHello() {
+    console.log("hello! my name is " + this.name)
+  }
+}
 
-// //creating an instance of the class Person
-// const daniel = new Person("daniel", 25, "Brasil", "rua 90")
-// const alex = new Person("alex", 28, "Portugal")
-// const lari = new Person("lari", 29, "Brasil")
+//creating an instance of the class Person
+const daniel = new Person("daniel", 25, "Brasil", "rua 90")
+const alex = new Person("alex", 28, "Portugal")
+const lari = new Person("lari", 29, "Brasil")
 
-// console.log(daniel.getaddress())
-// console.log("daniel object", daniel)
-// console.log("lari object", lari)
-// console.log("alex object", alex)
+console.log("daniel object", daniel)
+console.log("lari object", lari)
+console.log("alex object", alex)
 
-// daniel.sayHello()
-// lari.sayHello()
-// alex.sayHello()
-// console.log("Person", Person)
+daniel.sayHello()
+lari.sayHello()
+alex.sayHello()
+console.log("Person", Person)
 
-// class Animal {
-//   constructor(name, age, color) {
-//     this.name = name
-//     this.age = age
-//     this.color = color
-//   }
+class Animal {
+  constructor(name, age, color) {
+    this.name = name
+    this.age = age
+    this.color = color
+  }
 
-//   walk() {
-//     console.log(this.name + " is walking!")
-//   }
-// }
+  walk() {
+    console.log(this.name + " is walking!")
+  }
+}
 
-// class Dog extends Animal {
-//   constructor(name, age, color, breed) {
-//     super(name, age, color)
-//     this.dogBreed = breed
-//   }
-// }
+class Dog extends Animal {
+  constructor(name, age, color, breed) {
+    super(name, age, color)
+    this.dogBreed = breed
+  }
+}
 
-// const fred = new Dog("Fred", 4, "caramelo", "rotweiller")
+const fred = new Dog("Fred", 4, "caramelo", "rotweiller")
 
-// console.log("HERES FREDs INFORMATION", fred)
+console.log("HERES FREDs INFORMATION", fred)
 
-// fred.walk()
-// console.log(fred.name)
-// console.log(fred.age)
-// console.log(fred.dogBreed)
-// console.log(fred.color)
+fred.walk()
+console.log(fred.name)
+console.log(fred.age)
+console.log(fred.dogBreed)
+console.log(fred.color)
 
-// //creating a horse class, overwriting the walk() method
-// class Horse extends Animal {
-//   constructor(name, age, color, isCountry) {
-//     super(name, age, color)
-//     this.isCountry = isCountry
-//   }
+//creating a horse class, overwriting the walk() method
+class Horse extends Animal {
+  constructor(name, age, color, isCountry) {
+    super(name, age, color)
+    this.isCountry = isCountry
+  }
 
-//   walk() {
-//     console.log(this.name + " is galopando!")
-//   }
-// }
+  walk() {
+    console.log(this.name + " is galopando!")
+  }
+}
 
-// const myHorse = new Horse("Ted", 5, "black", true)
+const myHorse = new Horse("Ted", 5, "black", true)
 
-// myHorse.walk()
-// fred.walk()
+myHorse.walk()
+fred.walk()
 
 //PRIVATE AND STATIC
 
-class Person {
+class People {
   //static property, only accessible through class Person
   static isLive = true
   //private property, not accessible from outside the class
@@ -98,9 +97,9 @@ class Person {
 }
 
 //creating an instance of the class Person
-const someone = new Person("someone", 25, "Brasil", "rua 90")
+const someone = new People("someone", 25, "Brasil", "rua 90")
 
-console.log(Person.getStatus())
+console.log(People.getStatus())
 
 //returns the address which is private
 console.log(someone.fullAddress)
