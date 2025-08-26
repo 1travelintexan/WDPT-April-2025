a66a55bb1643eebd0c5076eef114dafd52b823de
Joshua <horheyinc@gmail.com>
2025-08-26 21:35:15 +0200

    auth routes

---
+++ module-3/week-3/day-1/module-3-project/server/routes/auth.routes.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const UserModel = require("../models/User.model");
+const bcryptjs = require("bcryptjs");
+//first a /signup route that will check for the user email if the email is free then create the user
+router.post("/signup", async (req, res) => {
+  try {
+    //check if the email already exists
+    const foundUser = await UserModel.findOne({ email: req.body.email });
+    if (foundUser) {
+      res.status(403).json({ errorMessage: "Email already taken" });
+    } else {
+      //create the salt
+      //create the hashed password
+      //create the hashed user variable without the original password
+      const theSalt = bcryptjs.genSaltSync(12);
+      const theHashedPassword = bcryptjs.hashSync(req.body.password, theSalt);
+      console.log("the salt", theSalt);
+      console.log("the hashed password", theHashedPassword);
+      console.log(req.body);
+      const hashedUser = {
+        ...req.body,
+        password: theHashedPassword,
+      };
+      const createdUser = await UserModel.create(hashedUser);
+      res
+        .status(201)
+        .json({ message: "User created successfully", createdUser });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("problem creating user");
+  }
+});
+//second /login route to find the user by their email, and if exists compare the passwords and then if the passwords match .... they are logged in
+router.post("/login", async (req, res) => {
+  try {
+    //find the person by their email
+    const foundUser = await UserModel.findOne({ email: req.body.email });
+    if (!foundUser) {
+      res.status(500).json({ errorMessage: "invalid credentials" });
+    } else {
+      //if the email exist then compare the passwords
+      const doesPasswordMatch = bcryptjs.compareSync(
+        req.body.password,
+        foundUser.password
+      );
+      if (!doesPasswordMatch) {
+        res.status(500).json({ errorMessage: "invalid credentials" });
+      } else {
+        res.status(200).json({ message: "You are logged in! Nice work" });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("problem signup user");
+  }
+});
+
+module.exports = router;
