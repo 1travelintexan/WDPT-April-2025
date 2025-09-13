76c345d30c748046e6cee590b2bc6bb930f768eb
Joshua <horheyinc@gmail.com>
2025-09-13 16:05:17 +0200

    cloudinary

--- module-3/week-3/day-1/module-3-project/server/models/Pet.model.js
+++ module-3/week-3/day-1/module-3-project/server/models/Pet.model.js
@@ -17,6 +17,11 @@ const petSchema = new Schema(
       ref: "User",
       required: true,
     },
+    image: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1497752531616-c3afd9760a11?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
--- module-3/week-3/day-1/module-3-project/server/routes/pets.routes.js
+++ module-3/week-3/day-1/module-3-project/server/routes/pets.routes.js
@@ -1,26 +1,33 @@
 const router = require("express").Router();
 const PetModel = require("../models/Pet.model");
 const UserModel = require("../models/User.model");
+const uploader = require("../middlewares/cloudinary.config");
 //route to add a new pet the DB and then... add that created pet to the array of an existing user
-router.post("/create-a-pet/:userId", async (req, res) => {
-  try {
-    //this creates the pet first
-    const createdPet = await PetModel.create({
-      ...req.body,
-      owner: req.params.userId,
-    });
-    //updates the user to have the pet in the array
-    const updatedUser = await UserModel.findByIdAndUpdate(
-      req.params.userId,
-      { $push: { pets: createdPet._id } },
-      { new: true }
-    );
-    res.status(201).json({ createdPet, updatedUser });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json("problem creating a pet ");
+router.post(
+  "/create-a-pet/:userId",
+  uploader.single("imageUrl"),
+  async (req, res) => {
+    console.log("req.file:", req.file);
+    try {
+      //this creates the pet first
+      const createdPet = await PetModel.create({
+        ...req.body,
+        owner: req.params.userId,
+        image: req.file?.path,
+      });
+      //updates the user to have the pet in the array
+      const updatedUser = await UserModel.findByIdAndUpdate(
+        req.params.userId,
+        { $push: { pets: createdPet._id } },
+        { new: true }
+      );
+      res.status(201).json({ createdPet, updatedUser });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json("problem creating a pet ");
+    }
   }
-});
+);
 
 //route to find all pets for a given user
 router.get("/user-pets/:userId", async (req, res) => {
